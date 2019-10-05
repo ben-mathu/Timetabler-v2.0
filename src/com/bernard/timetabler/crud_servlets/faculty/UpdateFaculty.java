bb42a564765da73bea9df34b31af298311284cb6
Benard Mathu <mathubenard@gmail.com>
2019-10-05 11:22:36 +0300

    Fix two servlets with same url-pattern

--- src/com/bernard/timetabler/crud_servlets/faculty/UpdateFaculty.java
+++ src/com/bernard/timetabler/crud_servlets/faculty/UpdateFaculty.java
@@ -21,7 +21,7 @@ import com.google.gson.Gson;
 /**
  * Servlet implementation class UpdateCourse
  */
-@WebServlet("/update-department")
+@WebServlet("/update-faculty")
 public class UpdateFaculty extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
