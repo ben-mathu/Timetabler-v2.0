36c7cd369103ab0594050feca8eda8c09fc506b9
Benard Mathu <mathubenard@gmail.com>
2019-11-12 18:19:07 +0300

    update procfile

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,3 @@
 mvn package
 rm -v /opt/tomcat/latest/webapps/Timetabler-v2.0-0.0.1-SNAPSHOT.war
 cp -v target/Timetabler-v2.0-0.0.1-SNAPSHOT.war /opt/tomcat/latest/webapps/
-tail -f /opt/tomcat/latest/logs/catalina.out
