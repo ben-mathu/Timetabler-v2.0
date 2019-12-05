72416002c03510f5d93ba02a946724ce8c5bbea8
Benard Mathu <mathubenard@gmail.com>
2019-12-05 21:19:47 +0300

    Fix: updating programmes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mvn clean install
-rm -v /opt/tomcat/latest/webapps/timetabler.war
-cp -v target/timetabler.war /opt/tomcat/latest/webapps/
+rm -v /opt/tomcat/latest/webapps/timetabler-1.0.0.war
+cp -v target/timetabler-1.0.0.war /opt/tomcat/latest/webapps/
 tail -fv /opt/tomcat/latest/logs/catalina.out
